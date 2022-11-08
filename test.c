6bb0153913ed2f6fa2f38664e9c58ec97895e175
Landyn Francis <landyn.francis@maine.edu>
2022-11-08 18:36:04 -0500

    test program successfully connects to my home WiFi

--- test.c
+++ test.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/gpio.h"
-#include "pico/binary_info.h"
 
-const uint LED_PIN = 25;
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+
+char ssid[] = "Telephone Line";
+char pass[] = "ElectricLightsOrchestra";
 
 int main(){
-        bi_decl(bi_program_description("This is a test binary."));
-        bi_decl(bi_1pin_with_name(LED_PIN,"On-board LED"));
-
         stdio_init_all();
-
-        gpio_init(LED_PIN);
-        gpio_set_dir(LED_PIN,GPIO_OUT);
-        while(1){
-                gpio_put(LED_PIN,0);
-                sleep_ms(250);
-                gpio_put(LED_PIN,1);
-                puts("Hello World\n");
-                sleep_ms(1000);
+        if (cyw43_arch_init_with_country(CYW43_COUNTRY_USA)) {
+                printf("Failed to initialize\n");
+                return 1;
         }
+        printf("Initialized\n");
+
+        cyw43_arch_enable_sta_mode();
+
+        if (cyw43_arch_wifi_connect_timeout_ms(ssid,pass,CYW43_AUTH_WPA2_AES_PSK, 10000)) {
+                printf("Failed to connect\n");
+                return 1;
+        }
+        printf("Connected\n"); 
 }
