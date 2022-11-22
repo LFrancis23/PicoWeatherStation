f0ea75ffa40af5a02c44d8b91b942656e732119d
Landyn Francis <landyn.francis@maine.edu>
2022-11-22 14:02:37 -0500

    stores data in SQL database now

--- client.py
+++ client.py
@@ -1,12 +1,52 @@
 #!/usr/bin/python3
 import socket
+import sqlite3
+from datetime import datetime
+import time
 
 HOST = "192.168.0.237"
 PORT = 42069
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST,PORT))
-    s.sendall(b"Requesting Data")
-    data = s.recv(1024)
 
-print(f"Received {data!r}")
+def getDateTime():
+	now = datetime.now()
+	now = now.isoformat()
+	return now
+
+def read_data():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST,PORT))
+        s.sendall(b"Requesting Data")
+        data = s.recv(1024)
+        data = data.decode("utf-8").split(',')
+    return data
+
+
+
+data = read_data()
+lux = data[0]
+temp = data[1]
+pressure = data[2]
+humidity = data[3]
+now = getDateTime()
+try:
+    sqliteConnection = sqlite3.connect('/var/www/datalogger/weatherstation.db')
+    cursor = sqliteConnection.cursor()
+    print ("Successfully connected to SQLITE")
+    cursor.execute("CREATE TABLE IF NOT EXISTS weather (lux REAL,temperature REAL,pressure REAL,humid REAL,datetime TEXT);")
+    sqliteConnection.commit()
+    print ("Successfully created table")
+    #Insert into table
+    count = cursor.execute("""INSERT INTO weather 
+                        (lux, temperature, pressure, humid, datetime) 
+                        VALUES (?,?,?,?,?)""",
+                        (lux,temp,pressure,humidity,now))
+    sqliteConnection.commit()
+    print("Record inserted successfully into weather table", cursor.rowcount)
+    cursor.close()
+except sqlite3.Error as error:
+    print("Error while creating sqlite table",error)
+finally:
+    if sqliteConnection:
+        sqliteConnection.close()
+        print("The SQLite Connection is closed")
